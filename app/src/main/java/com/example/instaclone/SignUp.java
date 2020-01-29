04a46bb9d8365ce7a1faf610e185e9fee5341ca5
Adedoyin <isradeoluwa@gmail.com>
2020-01-29 17:35:25 +0100

    Retrieving from parse server

--- app/src/main/java/com/example/instaclone/SignUp.java
+++ app/src/main/java/com/example/instaclone/SignUp.java
@@ -8,13 +8,16 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
 import java.util.PriorityQueue;
-
 
 public class SignUp extends AppCompatActivity implements View.OnClickListener{
 
@@ -22,7 +25,9 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
     private TextView nameEdtTxt;
     private TextView ageEdtTxt;
     private TextView clubEdtTxt;
-
+    private TextView getFootballerDetail;
+    private Button getAll;
+    private String getAllFootballers;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,8 +38,45 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener{
         nameEdtTxt = findViewById(R.id.fNameEdt);
         ageEdtTxt = findViewById(R.id.fAgeEdt);
         clubEdtTxt = findViewById(R.id.fClubEdt);
+        getFootballerDetail = findViewById(R.id.getDetailText);
+        getAll = findViewById(R.id.getAll);
 
         btnSave.setOnClickListener(SignUp.this);
+
+        getFootballerDetail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Footballer");
+                parseQuery.getInBackground("X7hvXoLFZm", new GetCallback<ParseObject>() {
+                    @Override
+                    public void done(ParseObject object, ParseException e) {
+                        getFootballerDetail.setText("Name: " + object.get("Name") + " \n" + "Club: " + object.get("Club") + " \n" + "Age: " + object.get("Age"));
+                    }
+                });
+
+            }
+        });
+
+        getAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ParseQuery<ParseObject> parseQuery = ParseQuery.getQuery("Footballer");
+                parseQuery.findInBackground(new FindCallback<ParseObject>() {
+                    @Override
+                    public void done(List<ParseObject> objects, ParseException e) {
+                        if (e == null){
+                            for (ParseObject footBaller : objects){
+                                getAllFootballers = getAllFootballers + footBaller.get("Name") + " \n";
+                            }
+                            FancyToast.makeText(SignUp.this, getAllFootballers,  FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
+                        } else {
+                            FancyToast.makeText(SignUp.this, e.getMessage(), FancyToast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
     }
 
