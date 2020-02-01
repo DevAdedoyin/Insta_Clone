c78409a83ab514671c9170eb14e799b317878867
Adedoyin <isradeoluwa@gmail.com>
2020-02-01 23:42:59 +0100

    Toolbar Set up

--- app/src/main/java/com/example/instaclone/Index.java
+++ app/src/main/java/com/example/instaclone/Index.java
@@ -5,19 +5,33 @@ import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
 import com.parse.ParseUser;
 
 public class Index extends AppCompatActivity {
 
-    private TextView welcomeText;
+    private Toolbar mToolbar;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private TabAdapter mTabAdapter;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.index_activity);
-        welcomeText = findViewById(R.id.indexView);
+        setTitle("Insta-Clone");
 
-        welcomeText.setText("Welcome " + ParseUser.getCurrentUser().get("username"));
+        mToolbar = findViewById(R.id.myToolbar);
+        setSupportActionBar(mToolbar);
+
+        mViewPager = findViewById(R.id.viewPager);
+        mTabAdapter = new TabAdapter(getSupportFragmentManager());
+        mViewPager.setAdapter(mTabAdapter);
+
+        mTabLayout = findViewById(R.id.tabLayout);
+        mTabLayout.setupWithViewPager(mViewPager, false);
     }
 }
